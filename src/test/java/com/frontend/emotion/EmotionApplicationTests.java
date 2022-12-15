a702b5048b1629861149fffe04d4c58a88bfecf6
aamoos <aamoos@naver.com>
2022-12-15 10:45:34 +0900

    Initial commit

---
+++ src/test/java/com/frontend/emotion/EmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.emotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
